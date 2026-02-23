b71c775ebebb09c83ba98c687e5b03eca2dc7c41
TXMMYBXY <altusha_03@bk.ru>
2026-02-23 14:02:49 +0700

    Это я забыл

--- MonitoringService.Api/Controllers/Statistic/VIewModels/DeleteStatisticViewModel.cs
+++ MonitoringService.Api/Controllers/Statistic/VIewModels/DeleteStatisticViewModel.cs
@@ -1,6 +1,9 @@
+using System.Text.Json.Serialization;
+
 namespace MonitoringService.Api.Controllers.Statistic.VIewModels;
 
 public class DeleteStatisticViewModel
 {
+    [JsonPropertyName(("id"))]
     public int Id { get; set; }
 }
--- MonitoringService.Api/Controllers/Statistic/VIewModels/GetDeviceViewModel.cs
+++ MonitoringService.Api/Controllers/Statistic/VIewModels/GetDeviceViewModel.cs
@@ -1,6 +1,9 @@
+using System.Text.Json.Serialization;
+
 namespace MonitoringService.Api.Controllers.Statistic.VIewModels;
 
 public class GetDeviceViewModel
 {
+    [JsonPropertyName("_id")]
     public string DeviceId { get; set; }
 }
